bad9f4ed5c9d4ab0a417f3274ebffee6cabaf090
Harry Marr <harry.marr@gmail.com>
2016-01-13 17:14:32 +0000

    logsearch v0.3

--- Formula/logsearch.rb
+++ Formula/logsearch.rb
@@ -1,9 +1,9 @@
 class Logsearch < Formula
   desc "Search Elasticsearch logs from the command line"
   homepage "https://github.com/gocardless/logsearch"
-  url "https://github.com/gocardless/logsearch/releases/download/v0.2/logsearch-0.2.darwin-amd64.tgz"
-  version "0.2"
-  sha256 "5dc7b0d106250b357cde7086026f9ed41de74a1ac3d20ee10a20b71900fd90e2"
+  url "https://github.com/gocardless/logsearch/releases/download/v0.3/logsearch-0.3.darwin-amd64.tgz"
+  version "0.3"
+  sha256 "660b7ac49a00cf9e47b1e7f56d81f610cc50efc8a5ebbf69fe49ac8a729a49f6"
 
   def install
     bin.install "logsearch"
