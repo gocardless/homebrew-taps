d0a68cde69feaf137de718816b69d11dd769a68d
Harry Maclean <harrymaclean@gocardless.com>
2017-09-14 15:57:07 +0100

    Bump draupnir to 0.1.2

--- Formula/draupnir.rb
+++ Formula/draupnir.rb
@@ -3,9 +3,9 @@
 class Draupnir < Formula
   desc "Client for the draupnir database service"
   homepage "https://github.com/gocardless/draupnir"
-  url "https://s3-eu-west-1.amazonaws.com/gocardless-tools/draupnir-client-0.1.1.zip"
-  sha256 "9dd6cd832a67ef8051fa7e162fe2fcff4387b3cefb4a10ef923189ac277d24cb"
-  version "0.1.1"
+  url "https://s3-eu-west-1.amazonaws.com/gocardless-tools/draupnir-client-0.1.2.zip"
+  sha256 "6df6cf5b7e6e211fb629842497427e47ba201dc6cbc311e0d5ddd13f46162f43"
+  version "0.1.2"
 
   def install
     bin.install "draupnir-client"
