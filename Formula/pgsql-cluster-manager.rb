75b05eccc862ddfa49c3fb7f331165c1a22c0058
GoCardless Engineering (goreleaser) <engineering@gocardless.com>
2017-12-05 18:00:23 +0000

    pgsql-cluster-manager version v0.0.6

--- Formula/pgsql-cluster-manager.rb
+++ Formula/pgsql-cluster-manager.rb
@@ -1,9 +1,9 @@
 class PgsqlClusterManager < Formula
   desc "Orchestrator for Postgres clustering with corosync/pacemaker/etcd"
   homepage "https://github.com/gocardless/pgsql-cluster-manager"
-  url "https://github.com/gocardless/pgsql-cluster-manager/releases/download/v0.0.5/pgsql-cluster-manager_0.0.5_darwin_amd64.tar.gz"
-  version "0.0.5"
-  sha256 "f81ada01d87b2a07f45120df17cf7ef96c8a6b7fd1defafbcc211f8088d028b5"
+  url "https://github.com/gocardless/pgsql-cluster-manager/releases/download/v0.0.6/pgsql-cluster-manager_0.0.6_darwin_amd64.tar.gz"
+  version "0.0.6"
+  sha256 "6a487048beac59a6445cd0015f0ba00c74017f08949b2be33efbd2d2d1bd51e5"
 
   def install
     bin.install "pgsql-cluster-manager"
