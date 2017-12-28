0c5f66be16c13d2c16cc75d8286c247b3f8cf7e1
Harry Maclean <harrymaclean@gocardless.com>
2017-12-28 12:59:51 +0000

    draupnir version v1.1.0

--- Formula/draupnir.rb
+++ Formula/draupnir.rb
@@ -1,11 +1,9 @@
-# frozen_string_literal: true
-
 class Draupnir < Formula
   desc "Client for the draupnir database service"
-  homepage "https://github.com/gocardless/draupnir"
-  url "https://s3-eu-west-1.amazonaws.com/gocardless-tools/draupnir-client-1.0.0.zip"
-  sha256 "a8c843f7f720562d16b7d93cbdcf81db704c9abbcf4aec6362e7a0156e6fe25b"
-  version "1.0.0"
+  homepage ""
+  url "https://github.com/gocardless/draupnir/releases/download/v1.1.0/draupnir_1.1.0_darwin_amd64.tar.gz"
+  version "1.1.0"
+  sha256 "17846ac0681533357cde15c6869c0b0f10438a070d6987a7326d365b0dda4213"
 
   def install
     bin.install "draupnir-client"
