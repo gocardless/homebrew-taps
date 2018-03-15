b03b9d8c169ed126cb6d0cd4e5e399390aaf0384
GoCardless Engineering <engineering@gocardless.com>
2018-03-15 09:33:55 +0000

    Brew formula update for anu version v0.0.5

--- Formula/anu.rb
+++ Formula/anu.rb
@@ -1,12 +1,16 @@
 class Anu < Formula
   desc "GoCardless Platform toolkit"
   homepage "https://github.com/gocardless/anu"
-  url "https://github.com/gocardless/anu/releases/download/v0.0.4/anu_0.0.4_darwin_amd64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-  version "0.0.4"
-  sha256 "d0a0b37c46abcd2ed23d23736e0f02e0b9770d5e428dae5718de306ab0c9eecb"
+  url "https://github.com/gocardless/anu/releases/download/v0.0.5/anu_0.0.5_darwin_amd64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+  version "0.0.5"
+  sha256 "578ccd49212800554f3f76c365d94089572e08ec2be0dd4b5237512fc16c29da"
 
   def install
     bin.install "anu"
+    
+    # Install shell auto-completion
+    (bash_completion/"anu").write(Utils.popen_read("#{bin}/anu completion bash"))
+    (zsh_completion/"_anu").write(Utils.popen_read("#{bin}/anu completion zsh"))
   end
 
   def caveats
