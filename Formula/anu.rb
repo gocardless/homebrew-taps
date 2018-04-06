b8a3c63d84e6e47412d42f2dc71d36043de6278e
GoCardless Engineering <engineering@gocardless.com>
2018-04-06 14:17:10 +0100

    Brew formula update for anu version v0.0.14

--- Formula/anu.rb
+++ Formula/anu.rb
@@ -1,9 +1,9 @@
 class Anu < Formula
   desc "GoCardless Platform toolkit"
   homepage "https://github.com/gocardless/anu"
-  url "https://github.com/gocardless/anu/releases/download/v0.0.13/anu_0.0.13_darwin_amd64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-  version "0.0.13"
-  sha256 "27b6122a2d9bb67115478b15cc9e3eb76051501f9a4e4d264ee46adc72aad611"
+  url "https://github.com/gocardless/anu/releases/download/v0.0.14/anu_0.0.14_darwin_amd64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+  version "0.0.14"
+  sha256 "0f383b75ab6ce44a52f9ccf32deb0321e9939651f7f9cfa54e6bf56dd53190fc"
 
   def install
     bin.install "anu"
