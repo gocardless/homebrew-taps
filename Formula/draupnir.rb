592b661cd318d9cdb239b2c7a025bfd868950d5c
GoCardless Engineering <engineering@gocardless.com>
2018-07-09 21:40:48 +0100

    Brew formula update for draupnir version v2.0.0

--- Formula/draupnir.rb
+++ Formula/draupnir.rb
@@ -1,9 +1,9 @@
 class Draupnir < Formula
   desc "Client for the draupnir database service"
   homepage ""
-  url "https://github.com/gocardless/draupnir/releases/download/v1.7.0/draupnir_1.7.0_darwin_amd64.tar.gz"
-  version "1.7.0"
-  sha256 "4da08196953964065226cbf71ccb6a6a7bfb4df2c6bf658cea9a66b0dc558366"
+  url "https://github.com/gocardless/draupnir/releases/download/v2.0.0/draupnir_2.0.0_darwin_amd64.tar.gz"
+  version "2.0.0"
+  sha256 "2cd8da94b264e6462eef4eadd5db16db44980e8e6b3a1c33cf9b9f1ae52a33ae"
 
   def install
     bin.install "draupnir-client"
