bcb6b948e3a48eb0ebc0cc895833d5ada2f29521
GoCardless Engineering <engineering@gocardless.com>
2018-10-23 16:50:22 +0100

    Brew formula update for codeowners version v0.0.3

--- Formula/gc-owners.rb
+++ Formula/gc-owners.rb
@@ -1,9 +1,9 @@
 class GcOwners < Formula
   desc "GoCardless code ownership tool"
   homepage "https://github.com/gocardless/gc-owners"
-  url "https://github.com/gocardless/codeowners/releases/download/v0.0.2/codeowners_0.0.2_darwin_amd64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-  version "0.0.2"
-  sha256 "43c68b5892ed5794e7207e74ddc2559c5c608bcfedd9051a55e863d2c105cedd"
+  url "https://github.com/gocardless/codeowners/releases/download/v0.0.3/codeowners_0.0.3_darwin_amd64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+  version "0.0.3"
+  sha256 "8d29d0ab05e4b75e00524fe5674f18819a0defee4a193350e66af20c81414ae5"
 
   def install
     bin.install "gc-owners"
