d5440ade81044ddc4d5591e3cec988cc5e56b77b
Harry Maclean <harrymaclean@gocardless.com>
2019-01-31 11:19:48 +0000

    Fix typo in release strategy constant

--- Formula/anu.rb
+++ Formula/anu.rb
@@ -3,7 +3,7 @@ require_relative "../lib/gc/github_private_release_download_strategy"
 class Anu < Formula
   desc "GoCardless Platform toolkit"
   homepage "https://github.com/gocardless/anu"
-  url "https://github.com/gocardless/anu/releases/download/v4.3.1/anu_4.3.1_darwin_amd64.tar.gz", :using => Gc::GitHubPrivateReleaseDownloadStrategy
+  url "https://github.com/gocardless/anu/releases/download/v4.3.1/anu_4.3.1_darwin_amd64.tar.gz", :using => Gc::GithubPrivateReleaseDownloadStrategy
   version "4.3.1"
   sha256 "27c86b899c415d0c4b13331df9c5a6785f6c2b99146c8ab873d38f09d6613730"
 
