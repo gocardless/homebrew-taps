991e53316cb22253de25cb50b0b6017f05c74ede
Lawrence Jones <lawrjone@gmail.com>
2019-02-05 10:01:39 +0000

    Draupnir v3.0.0

--- Formula/draupnir.rb
+++ Formula/draupnir.rb
@@ -3,15 +3,15 @@
 class Draupnir < Formula
   desc "Client for the draupnir database service"
   homepage ""
-  url "https://github.com/gocardless/draupnir/releases/download/v2.0.1/draupnir_2.0.1_darwin_amd64.tar.gz"
-  version "2.0.1"
-  sha256 "95fe8e17f0d298c096ca353fb6a23ddd3c8d98c00411a922afada00a35c87d75"
+  url "https://github.com/gocardless/draupnir/releases/download/v3.0.0/draupnir_3.0.0_darwin_amd64.tar.gz"
+  version "3.0.0"
+  sha256 "4bec566db73fec672ca6e51ad23fdfc9a367ab260016afa4b0a9dac77621c5a7"
 
   def install
-    bin.install "draupnir-client"
+    bin.install "draupnir"
   end
 
   test do
-    system "draupnir-client help"
+    system "draupnir help"
   end
 end
