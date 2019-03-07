83ae45fba489ba79951bb60f6be240638843b60a
Lawrence Jones <lawrjone@gmail.com>
2019-03-07 11:38:34 +0000

    Revert "Brew formula update for codeowners version v0.0.4"
    This reverts commit c865a49bb114264e880ee971b1855be83f458818.

--- Formula/gc-owners.rb
+++ Formula/gc-owners.rb
@@ -1,9 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "../lib/gc/github_private_release_download_strategy"
 class GcOwners < Formula
   desc "GoCardless code ownership tool"
   homepage "https://github.com/gocardless/gc-owners"
-  url "https://github.com/gocardless/codeowners/releases/download/v0.0.4/codeowners_0.0.4_darwin_amd64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-  version "0.0.4"
-  sha256 "7a9d418c919d7be0e8d2e30f694a827c5b1ca70cee39fddb82a77a2de0c4166a"
+  url "https://github.com/gocardless/codeowners/releases/download/v0.0.3/codeowners_0.0.3_darwin_amd64.tar.gz", using: Gc::GithubPrivateReleaseDownloadStrategy
+  version "0.0.3"
+  sha256 "8d29d0ab05e4b75e00524fe5674f18819a0defee4a193350e66af20c81414ae5"
 
   def install
     bin.install "gc-owners"
