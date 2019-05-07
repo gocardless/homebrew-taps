13516202232a02cd2855d882c9f70898144fe6dc
GoCardless Engineering <engineering@gocardless.com>
2019-05-07 16:02:39 +0100

    Brew formula update for anu version v5.1.0

--- Formula/anu.rb
+++ Formula/anu.rb
@@ -3,9 +3,9 @@ require_relative "../lib/gc/github_private_release_download_strategy"
 class Anu < Formula
   desc "GoCardless Platform toolkit"
   homepage "https://github.com/gocardless/anu"
-  url "https://github.com/gocardless/anu/releases/download/v5.0.4/anu_5.0.4_darwin_amd64.tar.gz", :using => Gc::GithubPrivateReleaseDownloadStrategy
-  version "5.0.4"
-  sha256 "ccf2938aee9cae2f3c2a40b2bd6baa78a6a5083f5aa8e86bf538401fddaba241"
+  url "https://github.com/gocardless/anu/releases/download/v5.1.0/anu_5.1.0_darwin_amd64.tar.gz", :using => Gc::GithubPrivateReleaseDownloadStrategy
+  version "5.1.0"
+  sha256 "57c85f7ad61f3c7c2b7fc915dfdb1149dd12e4eb8d2e9244277ee68ced6ba197"
   
   depends_on "kubernetes-cli"
 
