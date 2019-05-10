0b00e0e91bc3d3622d3c2ca8b1cdfc3db7c7922d
GoCardless Engineering (goreleaser) <engineering@gocardless.com>
2019-05-10 15:02:51 +0100

    Brew formula update for stolon-pgbouncer version v1.0.0

--- Formula/stolon-pgbouncer.rb
+++ Formula/stolon-pgbouncer.rb
@@ -2,9 +2,9 @@
 class StolonPgbouncer < Formula
   desc "Stolon add-on for running PgBouncer"
   homepage "https://github.com/gocardless/stolon-pgbouncer"
-  url "https://github.com/gocardless/stolon-pgbouncer/releases/download/v0.6.0/stolon-pgbouncer_0.6.0_darwin_amd64.tar.gz"
-  version "0.6.0"
-  sha256 "8fce6077cf57320246717d1d5868715911fe8ae03a4462e8466df2d057c74ac6"
+  url "https://github.com/gocardless/stolon-pgbouncer/releases/download/v1.0.0/stolon-pgbouncer_1.0.0_darwin_amd64.tar.gz"
+  version "1.0.0"
+  sha256 "0e2497e05224189a5091b651c0a7886dfaecf6f30c1789796959cfd5b615c1f3"
 
   def install
     bin.install "stolon-pgbouncer"
