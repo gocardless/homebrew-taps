e11f16ee824078e8499a9ba4f273609b191bb048
James Turley <jamesturley@gocardless.com>
2019-09-23 11:00:45 +0100

    Bump expose-host-agent to 0.0.2

--- Formula/expose-host-agent.rb
+++ Formula/expose-host-agent.rb
@@ -5,9 +5,8 @@ require_relative "../lib/gc/github_private_release_download_strategy"
 class ExposeHostAgent < Formula
   desc "Wrapper command that exposes your local SSH agent for use during docker builds"
   homepage "https://github.com/gocardless/expose-host-agent"
-  url "git@github.com:gocardless/expose-host-agent.git", :using => GitDownloadStrategy, :ref_type => :tag, :revision => "v0.0.1"
-  version "0.0.1"
-  sha256 "44418172f3803a988c4c114989259ffa7087298203afb7b7a3277fc30c65bb99"
+  url "git@github.com:gocardless/expose-host-agent.git", :using => GitDownloadStrategy, :ref_type => :tag, :revision => "v0.0.2"
+  version "0.0.2"
 
   depends_on "socat"
 
