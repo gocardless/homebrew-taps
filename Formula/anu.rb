a5f23953e8a4834595a7d38098af23a28b7c09aa
GoCardless Engineering <engineering@gocardless.com>
2020-06-22 17:08:18 +0100

    Brew formula update for anu version v10.2.0

--- Formula/anu.rb
+++ Formula/anu.rb
@@ -3,11 +3,21 @@ require_relative "../lib/gc/github_private_release_download_strategy"
 class Anu < Formula
   desc "GoCardless Platform toolkit"
   homepage "https://github.com/gocardless/anu"
-  url "https://github.com/gocardless/anu/releases/download/v10.0.0/anu_10.0.0_darwin_amd64.tar.gz", :using => Gc::GithubPrivateReleaseDownloadStrategy
-  version "10.0.0"
-  sha256 "7d0455bffd313f1a7c6cd9c6eb0ba363fa771bbe84d69257704391e9b1d89362"
+  version "10.2.0"
+  bottle :unneeded
+
+  if OS.mac?
+    url "https://github.com/gocardless/anu/releases/download/v10.2.0/anu_10.2.0_darwin_amd64.tar.gz", :using => Gc::GithubPrivateReleaseDownloadStrategy
+    sha256 "8f974f6d5ab3556049a1894d07845ad4cb8b02fa1b3189fe82c93d651b2f2b70"
+  elsif OS.linux?
+    if Hardware::CPU.intel?
+      url "https://github.com/gocardless/anu/releases/download/v10.2.0/anu_10.2.0_linux_amd64.tar.gz", :using => Gc::GithubPrivateReleaseDownloadStrategy
+      sha256 "a4a998e141392b0c9202afc67749034ec0dbf0aaa6267231d407ce86d4466c80"
+    end
+  end
   
   depends_on "kubernetes-cli"
+  depends_on "fzf"
 
   def install
     bin.install "anu"
@@ -17,6 +27,8 @@ class Anu < Formula
     # Install shell auto-completion
     (bash_completion/"anu").write(Utils.popen_read("#{bin}/anu completion bash"))
     (zsh_completion/"_anu").write(Utils.popen_read("#{bin}/anu completion zsh"))
+    (bash_completion/"utopia").write(Utils.popen_read("#{bin}/utopia --completion-script-bash"))
+    (zsh_completion/"_utopia").write(Utils.popen_read("#{bin}/utopia --completion-script-zsh"))
   end
 
   def caveats; <<~EOS
