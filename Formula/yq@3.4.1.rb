19d27141a3e3f826b13096caaf222b6f7cc3712c
Isaac Seymour <isaacseymour@users.noreply.github.com>
2020-12-22 19:03:32 +0000

    Fix yq@3.4.1 formula
    - Fix the download URL
    - use the tar not the zip (and update the sha)

--- Formula/yq@3.4.1.rb
+++ Formula/yq@3.4.1.rb
@@ -1,19 +1,14 @@
 class YqAT341 < Formula
   desc "Process YAML documents from the CLI"
   homepage "https://github.com/mikefarah/yq"
-  url "https://github.com/mikefarah/yq/archive/v3.4.1.tar.gz"
-  sha256 "db9a98525654d7c835989e88d9fcadaafc7b87965d691480cb0300bc2f3248ad"
-
-  bottle do
-    cellar :any_skip_relocation
-    sha256 "7eef86b614ab9695cb8ba108a659cba5cd504211b7236f8a561c7619aaf9b792" => :catalina
-    sha256 "5bf8a07349fcb306261676187d53d684cd14cfafead1a120a95a6318bb8beda9" => :mojave
-    sha256 "750248b8af5a72505593466f7add52fd6e20a2c2556dd7e28fc756420618680d" => :high_sierra
-  end
+  url "https://github.com/mikefarah/yq/archive/3.4.1.tar.gz"
+  sha256 "73259f808d589d11ea7a18e4cd38a2e98b518a6c2c178d1ec57d9c5942277cb1"
 
   depends_on "go" => :build
 
   conflicts_with "python-yq", :because => "both install `yq` executables"
+  conflicts_with "yq", :because => "both install `yq` executables"
+  conflicts_with "yq@2.4.1", :because => "both install `yq` executables"
 
   def install
     ENV["GOPATH"] = buildpath
