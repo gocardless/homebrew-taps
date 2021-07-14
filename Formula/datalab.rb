d9ef20095e7a5f4ab1dfc003e550ffe617739052
GoCardless Engineering <engineering@gocardless.com>
2021-07-14 14:16:36 +0100

    Brew formula update for datalab version v0.1.28

--- Formula/datalab.rb
+++ Formula/datalab.rb
@@ -3,9 +3,9 @@ require_relative "../lib/gc/github_private_release_download_strategy"
 class Datalab < Formula
   desc "GC Datalab - Tool to deploy and manage GC Datalab instances"
   homepage "https://github.com/gocardless/datalab"
-  url "https://github.com/gocardless/datalab/releases/download/v0.1.27/datalab_0.1.27_darwin_amd64.tar.gz", :using => Gc::GithubPrivateReleaseDownloadStrategy
-  version "0.1.27"
-  sha256 "847f901b1a820d6c605d5b574d54ce8414108e999799dded3051e859fce77d06"
+  url "https://github.com/gocardless/datalab/releases/download/v0.1.28/datalab_0.1.28_darwin_amd64.tar.gz", :using => Gc::GithubPrivateReleaseDownloadStrategy
+  version "0.1.28"
+  sha256 "38333477b4165a5bd9c100881586b0c01c6c75ef9eb879a763e9143506272b0c"
 
   def install
     bin.install "datalab"
