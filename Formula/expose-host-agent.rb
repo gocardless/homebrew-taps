c53686cac86c044f45425bf9f0eb60449f2e382b
James Turley <jamesturley@gocardless.com>
2021-11-16 11:31:26 +0000

    Use git sha revision for expose-host-agent
    Trying to fix weird checkout issues.

--- Formula/expose-host-agent.rb
+++ Formula/expose-host-agent.rb
@@ -3,7 +3,11 @@
 class ExposeHostAgent < Formula
   desc "Wrapper command that exposes your local SSH agent for use during docker builds"
   homepage "https://github.com/gocardless/expose-host-agent"
-  url "git@github.com:gocardless/expose-host-agent.git", :using => GitDownloadStrategy, :ref_type => :tag, :revision => "v0.0.3"
+  url "git@github.com:gocardless/expose-host-agent.git", 
+    :using => GitDownloadStrategy, 
+    :ref_type => :tag, 
+    :tag => "v0.0.3",
+    :revision => "7327da8eb2b0558e2a01b3fd46bec0c1b4550cf2"
   version "0.0.3"
 
   depends_on "socat"
