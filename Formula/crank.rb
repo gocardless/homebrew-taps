c83e557a66806060146780c5ebf7f373ef2472bd
jamiecobbett <jcobbett@gocardless.com>
2026-02-06 13:10:21 +0000

    Update crank to v1.2.0
    This update includes the resetExampleCounters function that was added
    in commit 16d2e9c on Nov 19, 2025. This function is needed by the
    gocardless-pro-go-template testdata generation.
    
    Previous version: vdaf95a2 (pre-Nov 2025)
    New version: v1.2.0 (released Jan 21, 2026)

--- Formula/crank.rb
+++ Formula/crank.rb
@@ -3,15 +3,15 @@ require_relative "../lib/gc/github_private_release_download_strategy"
 class Crank < Formula
   desc "GoCardless JSONSchema template generator"
   homepage "https://github.com/gocardless/crank"
-  version "4"
+  version "1.2.0"
 
   if OS.mac?
-    url "https://github.com/gocardless/crank/releases/download/vdaf95a2/crank_darwin_amd64", :using => Gc::GithubPrivateReleaseDownloadStrategy
-    sha256 "90d8c852c917d2ac15c0821bacc2e2835d8f7ff67cf4e750acb61ad43e9faf08"
+    url "https://github.com/gocardless/crank/releases/download/v1.2.0/crank_darwin_amd64", :using => Gc::GithubPrivateReleaseDownloadStrategy
+    sha256 "968ec943f71b14f714d65bbc36d3c22697cdc5870c8a84145756854fb83346a0"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/gocardless/crank/releases/download/vdaf95a2/crank_linux_amd64", :using => Gc::GithubPrivateReleaseDownloadStrategy
-      sha256 "0c77d0056a9d907f286febf6d9822825f492cf845341b41070623be2107b6ddb"
+      url "https://github.com/gocardless/crank/releases/download/v1.2.0/crank_linux_amd64", :using => Gc::GithubPrivateReleaseDownloadStrategy
+      sha256 "66dc402539b0b701f8f460df24521a4e38a9c823cbbc57b9df0e6ec9fd975625"
     end
   end
 
